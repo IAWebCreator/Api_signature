2957471c3b845db40c3b69f5f08282a5060c5c09
IAWebCreator <optimal.agentesia@gmail.com>
2024-11-02 00:30:48 -0500

    v6

--- app.py
+++ app.py
@@ -74,8 +74,8 @@ def add_signature():
         insert_position = fitz.Point(last_coord.x0, last_coord.y1 - 5)
         
         # Define the size of the signature (reduced by 25%)
-        sig_width = int(signature_image.width * 0.70)  # Reduce width by 25%
-        sig_height = int(signature_image.height * 0.70)  # Reduce height by 25%
+        sig_width = int(signature_image.width * 0.55)  # Reduce width by 25%
+        sig_height = int(signature_image.height * 0.55)  # Reduce height by 25%
 
         # Validate that the insertion point is within page bounds
         page = pdf_document[last_page]
